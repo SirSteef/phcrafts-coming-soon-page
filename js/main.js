00bab256a53f6a4cb6cf0314761a4fba97bf3b8c
steefteck <141339026+steefteck@users.noreply.github.com>
2024-01-08 17:50:28 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2024/01/17';
+        var finalDate = '2024/01/18';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
